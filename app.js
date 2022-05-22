b815c68eb823fdfc8101f82468102531a8102021
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2022-05-22 12:36:44 +0100

    crew

---
+++ app.js
@@ -0,0 +1,61 @@
+const feathers = require('@feathersjs/feathers')
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
+const app = express(feathers());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.configure(express.rest());
+app.configure(socketio());
+
+
+class MessageService {
+    constructor(){
+        this.messages = [];
+    }
+
+    async find(){
+        return this.messages
+    }
+
+    async create(data){
+        const message = {
+            id: this.messages.length,
+            text: data.text
+        }
+
+        this.messages.push(message);
+        return message;
+    }
+}
+
+
+app.use("messages", new MessageService());
+
+
+
+app.service("messages").on('created', (message)=>{
+    console.log('A new message has been created ', message)
+});
+
+
+
+app.use('/messages', new MessageService());
+app.use(express.errorHandler());
+
+
+
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
+);
+app.publish(data => app.channel('everybody'));
+
+
+
+app.listen(3030).on('listening', () =>
+  console.log('Feathers server listening on localhost:3030')
+);
+
+app.service('messages').create({
+    text: 'Hello world from the server'
+  });
